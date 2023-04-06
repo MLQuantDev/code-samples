2e3b01a740bb215d1f5f7fd0bb77921a68fb3c24
MLQuantDev <130053903+MLQuantDev@users.noreply.github.com>
2023-04-06 13:59:29 +0300

    Update print_table.py
    improved table formatting

--- print_table.py
+++ print_table.py
@@ -19,14 +19,17 @@ for product in data:
     max_value_length = max(len(str(value)) for value in product.values())
     value_widths.append(max_value_length)
 
+# Determine the maximum key length for formatting
+max_key_length = max(len(key) for key in data[0].keys())
+
 # Print header
-header = ''
+header = ' ' * max_key_length + ' | '
 for i, width in enumerate(value_widths):
     header += f"{data[i]['Product']:{width}s} | "
 print(header.rstrip())
 
 # Print separator
-separator = ''
+separator = '-' * max_key_length + '-+-'
 for width in value_widths:
     separator += '-' * width + '-+-'
 print(separator.rstrip())
@@ -37,4 +40,14 @@ for key in keys[1:]:
     row = ''
     for i, width in enumerate(value_widths):
         row += f"{str(data[i][key]):{width}s} | "
-    print(f"{key} | {row.rstrip()}")
+    print(f"{key:{max_key_length}s} | {row.rstrip()}")
+
+    
+
+### Output looks like this
+#          | Product 1 | Product 2
+#----------+-----------+----------
+#Price     | 120.5     | 199.99
+#Quantity  | 15        | 8
+#In Stock  | True      | False
+
